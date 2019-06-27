1b0fed3c3ce3cf27500ecc2620816c16eb40aa02
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-27 20:29:05 +0200

    subscribe to chain

--- src/lib/client.js
+++ src/lib/client.js
@@ -4,12 +4,17 @@ const address = 'ws://localhost:8080';
 
 module.exports = {
   start: () => {
+    let subscribed = false;
     return new Promise((resolve, reject) => {
       const socket = new WebSocket(address);
 
       socket.on('open', () => {
         console.log(`Conected to substrate-telemetry on ${address}`);
         resolve();
+      });
+
+      socket.on('close', () => {
+        console.log(`Conection to substrate-telemetry on ${address} closed`);
       });
 
       socket.on('error', (err) => {
@@ -19,6 +24,12 @@ module.exports = {
 
       socket.on('message', (data) => {
         console.log(data);
+        const parsedData = JSON.parse(data);
+        if (!subscribed) {
+          const chain = parsedData[3][0];
+          socket.send(`subscribe:${chain}`);
+          subscribed = true;
+        }
       });
     });
   }
