cb1003da5e996944a0561a7e3d48b48527c14ae4
Federico Gimenez <federico.gimenez@gmail.com>
2019-06-27 20:06:31 +0200

    start client and wait for it connected with backoff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+const Backoff = require('backoff-promise');
 const express = require('express');
 
+const client = require('./lib/client');
 const prometheus = require('./lib/prometheus');
 
 const app = express();
 const port = 3000;
+const backoff = new Backoff();
 
-prometheus.injectMetricsRoute(app);
-prometheus.startCollection();
+async function start() {
+  prometheus.injectMetricsRoute(app);
+  prometheus.startCollection();
 
-app.listen(port, () => console.log(`substrate-telemtry-exporter listening on port ${port}!`))
+  await backoff.run(() => {
+    return client.start();
+  });
+
+  app.listen(port, () => console.log(`substrate-telemtry-exporter listening on port ${port}`))
+}
+
+start();
